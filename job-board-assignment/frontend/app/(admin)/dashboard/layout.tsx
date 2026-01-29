8a1cf82316d38536154f639e6a7ab46a89cc3ccd
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-01-29 17:47:09 +0200

    feat!: update dashboard layout

--- job-board-assignment/frontend/app/(admin)/dashboard/layout.tsx
+++ job-board-assignment/frontend/app/(admin)/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from "sonner";
+
 export default function AdminLayout({
   children,
   sidebar,
@@ -8,12 +10,16 @@ export default function AdminLayout({
   navbar: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full bg-slate-50">
-      {sidebar}
-      <div className="flex-1 flex flex-col">
-        {navbar}
-        <main className="flex-1 px-4 py-6 md:px-6">{children}</main>
+    <>
+      <Toaster position="top-center" richColors />
+
+      <div className="flex h-full bg-slate-50">
+        {sidebar}
+        <div className="flex-1 flex flex-col">
+          {navbar}
+          <main className="flex-1 px-4 py-6 md:px-6">{children}</main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
