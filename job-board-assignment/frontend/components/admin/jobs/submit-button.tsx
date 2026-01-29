0db582b72238e39efa2d763db034c0e1b67605ca
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-01-29 17:50:08 +0200

    feat: add alert dialog shadcn component

---
+++ job-board-assignment/frontend/components/admin/jobs/submit-button.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+
+export default function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return <Button disabled={pending}>{pending ? "Saving..." : "Submit"}</Button>;
+}
